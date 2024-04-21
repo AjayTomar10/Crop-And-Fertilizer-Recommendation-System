c972ad9f2dc64726ffbaa646debacd8e9dd4dcc9
AjayTomar10 <tomarajay262@gmail.com>
2024-04-21 20:36:08 +0530

    https://github.com/AjayTomar10/Mini_project

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 # =========================================================================
-
+@app.route('/crop')
+def crop_recommendation():
+    return render_template("crop.html")
 @app.route("/predict",methods=['POST'])
 def predict():
     N = request.form['Nitrogen']
@@ -27,20 +29,20 @@ def predict():
     rainfall = request.form['Rainfall']
 
     
-
+    result=["","",""]
     prediction = recommendation(N, P, K, temp, humidity, ph, rainfall)
 
     crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
                  8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
                  14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
                  19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
-
-    if prediction[0] in crop_dict:
-        crop = crop_dict[prediction[0]]
-        result = "{} is the best crop to be cultivated right there".format(crop)
-    else:
-        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
-    return render_template('index.html',result = result)
+    for i in range(3):
+        if prediction[i] in crop_dict:
+            crop = crop_dict[prediction[i]]
+            result[i] = "{} is the best crop to be cultivated right there".format(crop)
+        else:
+            result[i] = "Sorry, we could not determine the best crop to be cultivated with the provided data."
+    return render_template('crop.html',result = result)
 
 
 # Recoomendation Model
@@ -91,10 +93,9 @@ def recommendation(N,P,K,temperature,humidity,ph,rainfall):
     df = crop.copy()  # Make a copy of the DataFrame to avoid modifying the original data
     df['crop_num'] = df['label'].map(crop_dict)
     df.drop('label', axis=1, inplace=True)
-    for i in range(1):
+    for i in range(3):
         val=prediction_model(df,features)
         result.append(val)
-        print(val)
         df=df[df['crop_num'] != val]
     
     return result
