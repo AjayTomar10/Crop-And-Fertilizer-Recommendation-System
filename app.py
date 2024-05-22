f80d574e1d653028bb60011680637a0835bfc960
pythongit <nayak4nayak4@gmail.com>
2024-05-22 22:35:17 +0530

    Updated app and templates, added crop images

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from flask import Flask,request,render_template
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('index.html')
 def index():
     return render_template("index.html")
 # =========================================================================
